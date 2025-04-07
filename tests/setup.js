e69a97a904e8accedab29db460415999a0234660
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-04-07 09:56:32 +0200

    add basic mongoose setup

---
+++ tests/setup.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+let mongod;
+
+// Connect to the in-memory database before running tests
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+  await mongoose.connect(uri);
+});
+
+// Clear all data after each test
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany();
+  }
+});
+
+// Close database connection after all tests
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongod.stop();
+});
