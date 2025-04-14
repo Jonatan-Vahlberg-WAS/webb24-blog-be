79e7b9b8e91a1a68634ccc3b5188627cbde46ad5
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-04-14 10:03:57 +0200

    simplify admin middleware

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,30 +1,11 @@
-const { verifyAccessToken } = require("../utils/jwt");
-
-function adminMiddleware(req, res, next) {
-  try {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
-      throw new Error("No authorization");
-    }
-
-    const token = authorization.split(" ")?.[1];
-    if (!token) {
-      throw new Error("No token");
-    }
-    const decyptedToken = verifyAccessToken(token);
-    if(!decyptedToken.isAdmin) {
-      throw new Error("Not an admin")
-    }
-    req.userId = decyptedToken.userId
-    req.isAdmin = decyptedToken.isAdmin || false
-    next();
-    return;
-  } catch (error) {
-    console.warn("Error: authroizing endpoint", error)
+function adminMiddleware(req,res,next) {
+  const isAdmin = req.isAdmin;
+  if(!isAdmin) {
     return res.status(401).json({
       message: "Unauthorized",
     });
   }
+  next()
 }
 
 module.exports = adminMiddleware
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,10 +5,13 @@ const User = require("../models/user.model");
 const authMiddleware = require("../middlewares/authMiddleware");
 const adminMiddleware = require("../middlewares/adminMiddleware");
 
+const userSelect = ["email", "name", "isAdmin"]
+
+
 router.get("/me", authMiddleware, async (req, res) => {
     const userId = req.userId;
     try {
-        const user = await User.findById(userId).select("-password")
+        const user = await User.findById(userId).select(userSelect)
         if(!user) {
             throw new Error("Unauthorized")
         }
@@ -22,7 +25,7 @@ router.get("/me", authMiddleware, async (req, res) => {
     }
 })
 
-router.get("/admin/all/", adminMiddleware, async (req, res) => {
+router.get("/admin/all/",authMiddleware, adminMiddleware, async (req, res) => {
     try {
         const users = await User.find().select("-password")
         return res.json(users)
